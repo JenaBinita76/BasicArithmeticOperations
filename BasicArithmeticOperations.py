d393725d5f9caa6fe0d22704c9727f3ab042b9ab
JenaBinita76 <78972198+JenaBinita76@users.noreply.github.com>
2021-02-14 00:09:23 +0530

    Add files via upload

---
+++ BasicArithmeticOperations.py
@@ -0,0 +1,14 @@
+num1=input("Enter the first number:")
+num2=input("Enter the second number:")
+#Addition
+s=int(num1)+int(num2)
+print("Sum of "+num1+" and "+num2+" is "+str(s))
+#Subtraction
+d=int(num1)-int(num2)
+print("Difference of "+num1+" and "+num2+" is "+str(d))
+#Multiplication
+p=int(num1)*int(num2)
+print("Product of "+num1+" and "+num2+" is "+str(p))
+#Division
+q=float(num1)/float(num2)
+print("Division of "+num1+" by "+num2+" gives "+str(q))
